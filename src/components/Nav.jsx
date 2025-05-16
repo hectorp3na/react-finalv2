1951a953404e0759a960e601d0f5cb3900ad4e75
Hector <hectorpena6590@gmail.com>
2025-05-15 20:28:10 -0700

    menu button

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/b7438e6d-4fe4-46d1-9917-4a204a998d59.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 const Nav = () => {
- function openMenu() {
-  document.body.classList += " menu--open";
- }
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [menuVisible, setMenuVisible] = useState(false);
 
- function closeMenu() {
-  document.body.classList.remove("menu--open");
- }
+  const openMenu = () => {
+    setMenuVisible(true);
+    setTimeout(() => setMenuOpen(true), 10); // Slight delay for transition
+  };
+  
+  const closeMenu = () => {
+    setMenuOpen(false);
+    setTimeout(() => setMenuVisible(false), 300); // Match transition duration
+  };
+
 
   return (
     <nav className="navbar">
@@ -33,34 +39,36 @@ const Nav = () => {
         </li>
       </ul>
 
-      <button
-        className="btn__menu" onClick={openMenu}
-      >
+      <button className="btn__menu" onClick={openMenu}>
         <FontAwesomeIcon icon={faBars} />
       </button>
 
-      <div className="menu__backdrop">
-        <button className="btn__menu btn__menu--close" onClick={closeMenu}>
-          <FontAwesomeIcon icon={faXmark} />
-        </button>
-        <ul className="menu__links">
-          <li className="menu__list">
-            <a href="/" className="menu__link" >
-              Home
-            </a>
-          </li>
-          <li className="menu__list">
-            <a href="/movies" className="menu__link" >
-              Search Movies
-            </a>
-          </li>
-          <li className="menu__list">
-            <a href="/" className="menu__link" >
-              Contact
-            </a>
-          </li>
-        </ul>
-      </div>
+      {menuVisible && (
+        <div className={`menu__backdrop ${menuOpen ? "slide-in" : "slide-out"}`}>
+          <button
+            className="btn__menu btn__menu--close"
+            onClick={closeMenu}>
+            <FontAwesomeIcon icon={faXmark} />
+          </button>
+          <ul className="menu__links">
+            <li className="menu__list">
+              <a href="/" className="menu__link" onClick={closeMenu}>
+                Home
+              </a>
+            </li>
+            <li className="menu__list">
+              <a href="/movies" className="menu__link" onClick={closeMenu}>
+                Search Movies
+              </a>
+            </li>
+            <li className="menu__list">
+              <a href="/" className="menu__link" onClick={closeMenu}>
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 };
